6d4cd5da33224f7457b2f428e9e40d9ce5fa9514
Rohit Ganguly <rohitganguly450@gmail.com>
2025-05-29 01:35:56 +0530

    remove the console.log

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -10,8 +10,6 @@ export const isAuthenticated = TryCatch((req, res, next) => {
       return next(new ErrorHandler("Please login to access this route", 401));
   
     const decodedData = jwt.verify(token, process.env.JWT_SECRET);
-  
-    console.log("", decodedData);
     req.user = decodedData;
   
     next();
